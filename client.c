56ff39eb66f04e44828e970c6845045620c704fd
UULi <denny99203@gmail.com>
2024-12-02 14:49:45 +0800

    feat: add some test command to client.c

--- client.c
+++ client.c
@@ -3,6 +3,14 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
+// Test commands
+#define TEST_COMMAND1 "create homework2.c rwr---"
+#define TEST_COMMAND2 "read homework2.c"
+#define TEST_COMMAND3 "write homework2.c o"
+#define TEST_COMMAND4 "write homework2.c a"
+#define TEST_COMMAND5 "write homework2.c rw----"
+
+
 
 int main(void) {
     

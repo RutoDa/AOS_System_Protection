1cf42b17d6896eba4f28d1d5c6a322053d9e2571
UULi <denny99203@gmail.com>
2024-12-02 14:51:40 +0800

    feat: Completed capability

--- capability.c
+++ capability.c
@@ -9,7 +9,7 @@ Group* create_group(Groups *groups, const char *group_name) {
         fprintf(stderr, "Error: Unable to allocate memory for group\n");
         exit(1);
     }
-    strncpy(group->group_name, group_name, strlen(group_name)+1);
+    strncpy(group->name, group_name, strlen(group_name)+1);
     group->capability_list = malloc(sizeof(CapabilityList));
     if (group->capability_list == NULL) {
         fprintf(stderr, "Error: Unable to allocate memory for group capabilities\n");
@@ -28,6 +28,57 @@ Group* create_group(Groups *groups, const char *group_name) {
     }
     groups->count++;
     return group;
+}
+
+Group* find_group_by_name(Groups *groups, const char *group_name) {
+    for (Group* group = groups->head; group != NULL; group = group->next) {
+        if (!strcmp(group->name, group_name))
+            return group;
+    }
+    return NULL;
+}
+
+User* find_user_by_name(Users *users, const char *user_name) {
+    for (User* user = users->head; user != NULL; user = user->next) {
+        if (!strcmp(user->name, user_name)) 
+            return user;
+    }
+    return NULL;
+}
+
+User* create_user(Users *users, Group *groups, char *username, char *group_name) {
+    User *user = malloc(sizeof(User));
+    if (user == NULL) {
+        fprintf(stderr, "Error: Unable to allocate memory for user\n");
+        exit(1);
+    }
+    strncpy(user->name, username, strlen(username)+1);
+    
+    Group* group = find_group_by_name(groups, group_name);
+    if (group == NULL) {
+        fprintf(stderr, "Error: Group %s not found\n", group_name);
+        exit(1);
+    }
+
+    user->group = group;
+    user->capability_list = malloc(sizeof(CapabilityList));
+    if (user->capability_list == NULL) {
+        fprintf(stderr, "Error: Unable to allocate memory for user capabilities\n");
+        exit(1);
+    }
+    user->capability_list->head = NULL;
+    user->capability_list->tail = NULL;
+    user->capability_list->count = 0;
+    user->next = NULL;
+    if (users->head == NULL) {
+        users->head = user;
+        users->tail = user;
+    } else {
+        users->tail->next = user;
+        users->tail = user;
+    }
+    users->count++;
+    return user;
 }
 
 Groups* init_groups(void) {
@@ -52,4 +103,39 @@ Users* init_users(void) {
     users->tail = NULL;
     users->count = 0;
     return users;
+}
+
+
+void free_system(Groups *groups, Users *users) {
+    Group *group = groups->head;
+    while (group != NULL) {
+        Group *next_group = group->next;
+        
+        while (group->capability_list->head != NULL) {
+            Capability *capability = group->capability_list->head;
+            group->capability_list->head = capability->next;
+            free(capability);
+        }
+
+        free(group->capability_list);
+        free(group);
+        group = next_group;
+    }
+    free(groups);
+    
+    User *user = users->head;
+    while (user != NULL) {
+        User *next_user = user->next;
+
+        while (user->capability_list->head != NULL) {
+            Capability *capability = user->capability_list->head;
+            user->capability_list->head = capability->next;
+            free(capability);
+        }
+
+        free(user->capability_list);
+        free(user);
+        user = next_user;
+    }
+    free(users);
 }
--- capability.h
+++ capability.h
@@ -1,18 +1,39 @@
 /*
     1. This file contains the declaration of the capability list data structure.
     2. The capability list's structure is as follows:
-        Users (linked list)
-            - username
-            - groups (linked list)
-                - group_name
-                - capabilities (linked list)
-                    - file_name
-                    - read_permission
-                    - write_permission
-            - capabilities (linked list)
-                - file_name
-                - read_permission
-                - write_permission
+        User 
+            - name: The name of the user
+            - group: Pointer to the group the user belongs to
+            - capability_list: Pointer to the capability list for the user
+            - next: Pointer to the next user in the list
+        Users
+            - head: Pointer to the first user in the list
+            - tail: Pointer to the last user in the list
+            - count: The number of users in the list
+        Group
+            - name: The name of the group
+            - capability_list: Pointer to the capability list for the group
+            - next: Pointer to the next group in the list
+        Groups
+            - head: Pointer to the first group in the list
+            - tail: Pointer to the last group in the list
+            - count: The number of groups in the list
+        Capability_List
+            - head: Pointer to the first capability in the list
+            - tail: Pointer to the last capability in the list
+            - count: The number of capabilities in the list
+        Capability
+            - file: The file the capability is for
+            - read_permission: Permissions for the owner, group, and others
+            - write_permission: Permissions for the owner, group, and others
+            - next: Pointer to the next capability in the list
+        File
+            - file_name: The name of the file
+            - rwlock: The lock for the file
+        Files
+            - head: Pointer to the first file in the list
+            - tail: Pointer to the last file in the list
+            - count: The number of files in the list
     3. Another diagram is as follows:
         user -> group -> capability
         |
@@ -25,10 +46,29 @@
 #ifndef CAPABILITY_H
 #define CAPABILITY_H
 #include <stdbool.h>
+#include <pthread.h>
 
-typedef struct Capability {
+typedef struct File {
     // The name of the file
     char file_name[256];
+    // The lock for the file
+    pthread_rwlock_t rwlock;
+} File;
+
+
+typedef struct Files {
+    // Pointer to the first file in the list
+    File *head;
+    // Pointer to the last file in the list
+    File *tail;
+    // The number of files in the list
+    int count;
+} Files;
+
+
+typedef struct Capability {
+    // The file the capability is for
+    File *file;
     // Permissions for the owner, group, and others
     bool read_permission;
     bool write_permission;
@@ -48,7 +88,7 @@ typedef struct CapabilityList {
 
 typedef struct Group {
     // The name of the group
-    char group_name[256];
+    char name[256];
     // Pointer to the capability list for the group
     CapabilityList *capability_list;
     // Pointer to the next group in the list
@@ -66,11 +106,11 @@ typedef struct Groups {
 
 typedef struct User {
     // The name of the user
-    char username[256];
+    char name[256];
     // Pointer to the group the user belongs to
-    Groups *groups;
+    Group *group;
     // Pointer to the capability list for the user
-    CapabilityList *capabilities;
+    CapabilityList *capability_list;
     // Pointer to the next user in the list
     struct User *next;
 } User;
@@ -84,12 +124,16 @@ typedef struct Users {
     int count;
 } Users;
 
-bool create_user(User *user, char *username, char *group_name);
+User* create_user(Users *users, Group *groups, char *username, char *group_name);
+
+Group* find_group_by_name(Groups *groups, const char *group_name);
 
 Group* create_group(Groups *groups, const char *group_name);
 
 Groups* init_groups(void);
 
 Users* init_users(void);
+
+void free_system(Groups *groups, Users *users);
 
 #endif 
